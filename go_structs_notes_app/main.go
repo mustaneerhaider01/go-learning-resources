7fffb7297f67b9b591631cf6957b3c5bbac0f20d
Mustaneer Haider <mostanirha1der19@gmail.com>
2024-04-27 17:10:16 +0500

    interfaces and any type added

--- go_structs_notes_app/main.go
+++ go_structs_notes_app/main.go
@@ -7,10 +7,33 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
 
+type saver interface {
+	Save() error
+}
+
+type outputtable interface {
+	saver
+	Display()
+}
+
 func main() {
+	printSomething("Welcome")
+	printSomething(1)
+	printSomething(true)
+	printSomething(2.5)
+
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text:")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	userNote, err := note.New(title, content)
 
@@ -19,15 +42,35 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
+	err = outputData(todo)
 
 	if err != nil {
-		fmt.Println("Saving the note failed.")
 		return
 	}
 
-	fmt.Println("Saving the note succeeded!")
+	outputData(userNote)
+}
+
+// interface{} can also be replaced with "any" since it's an alias for it.
+func printSomething(value interface{}) {
+	fmt.Println(value)
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Saving the data failed.")
+		return err
+	}
+
+	fmt.Println("Saving the data succeeded!")
+	return nil
 }
 
 func getNoteData() (string, string) {
