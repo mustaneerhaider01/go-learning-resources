bb66bfe28e4278902ae0f0649300bfc98f0e82b2
Mustaneer Haider <mostanirha1der19@gmail.com>
2024-04-28 11:56:38 +0500

    added type switch and extract type info

--- go_structs_notes_app/main.go
+++ go_structs_notes_app/main.go
@@ -22,7 +22,6 @@ type outputtable interface {
 func main() {
 	printSomething("Welcome")
 	printSomething(1)
-	printSomething(true)
 	printSomething(2.5)
 
 	title, content := getNoteData()
@@ -36,6 +35,10 @@ func main() {
 	}
 
 	userNote, err := note.New(title, content)
+
+	// We can pass our todo struct value to this function but it won't do anything since we
+	// don't hande that Todo struct type inside this printSomething()
+	printSomething(todo)
 
 	if err != nil {
 		fmt.Println(err)
@@ -53,7 +56,39 @@ func main() {
 
 // interface{} can also be replaced with "any" since it's an alias for it.
 func printSomething(value interface{}) {
-	fmt.Println(value)
+	// ok will indicate if value is of that type specified inside parenthesis.
+	// intVal will be that value but now Go will know that it's of that type mentioned in ()
+	// Alternative to Type Switch for executing different code block based on a specific type
+	intVal, ok := value.(int)
+
+	if ok {
+		fmt.Println("Integer:", intVal)
+		return
+	}
+
+	floatVal, ok := value.(float64)
+
+	if ok {
+		fmt.Println("Float:", floatVal)
+		return
+	}
+
+	stringVal, ok := value.(string)
+
+	if ok {
+		fmt.Println("String:", stringVal)
+		return
+	}
+	// Type Switch
+	// value.(type) works only with switch statement as it's a special use case in Go
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Printf("Integer: %v\n", value)
+	// case float64:
+	// 	fmt.Printf("Float: %v\n", value)
+	// case string:
+	// 	fmt.Printf("String: %v\n", value)
+	// }
 }
 
 func outputData(data outputtable) error {
