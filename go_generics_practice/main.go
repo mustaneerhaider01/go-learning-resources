5648c9042b56e8374e7fa1de735b6cc3fc36f5bb
Mustaneer Haider <mostanirha1der19@gmail.com>
2024-04-28 12:35:13 +0500

    added comments

--- go_generics_practice/main.go
+++ go_generics_practice/main.go
@@ -11,12 +11,19 @@ func (text str) log() {
 }
 
 func main() {
+	// Since both param types is int so Go correclty infers the return type here as 'int'
 	result := add(5, 10)
 
+	// first convert result to string as direct coversion to str() wil yield a 'rune'
+	// Then covert string to str()
 	resultLog := str(fmt.Sprint(result))
 	resultLog.log()
 }
 
+// T refers to type placeholder and we have list out specific type that can be filled in for T
+// The type for T will be set when we call this add()
+// T basically is a union of these three types and will not accept value of any other type, so both
+// paramters to this add() should of these listed types.
 func add[T int | float64 | string](a, b T) T {
 	return a + b
 }
