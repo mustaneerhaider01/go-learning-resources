f52431dff4eac5258b90b2b0fee0ed903d93bdb2
Mustaneer Haider <mostanirha1der19@gmail.com>
2024-05-13 19:33:15 +0500

    recursion code moved to package

--- go-functions-deep-dive/main.go
+++ go-functions-deep-dive/main.go
@@ -1,20 +1,3 @@
 package main
 
-import "fmt"
-
-func main() {
-	fact := factorial(7)
-	fmt.Println(fact)
-}
-
-// Recursion is when function call itself.
-// The function can't finish it's execution unless the inner function(s) it calls return their values
-// and finish their execution.
-// When a function recursively call itself, there should be a terminating condition to end the function
-// execution else this will create infinite loop.
-func factorial(num int) int {
-	if num == 0 {
-		return 1
-	}
-	return num * factorial(num-1)
-}
+func main() {}
