d7735504b35764e8f4871536d2d524f0d05806a8
Mustaneer Haider <mostanirha1der19@gmail.com>
2024-05-18 16:59:40 +0500

    updated

--- go_price_calculator_project/main.go
+++ go_price_calculator_project/main.go
@@ -11,6 +11,10 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
+		// Swappable structs funtionality can be achieved via an interface.
+		// So every struct that adheres to that interface can be used as the value.
+		// Both fm and cmdm struct values, adheres to IOManager interface which is what required by
+		// the NewTaxIncludedPriceJob() method as the first parameter.
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		// cmdm := cmdmanager.New()
 
