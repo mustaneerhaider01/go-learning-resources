67ddc0576b5b92ce6a11f1501c541d1f51c67a09
Mustaneer Haider <mostanirha1der19@gmail.com>
2024-05-26 10:45:18 +0500

    Added POST create event API

---
+++ go_rest_api/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":4000") // localhost:4000
+}
+
+func getEvents(ctx *gin.Context) {
+	events := models.GetAllEvents()
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Couldn't parse the request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
+}
